90ce05528369c8e316af45bd56dfbd2350720a6b
michal <dsw59799@student.dsw.edu.pl>
2025-06-26 19:45:23 +0200

    Dodano obsługę --logs z parametrem

--- skrypt.sh
+++ skrypt.sh
@@ -4,7 +4,8 @@ if [ "$1" == "--date" ]; then
   date
 
 elif [ "$1" == "--logs" ]; then
-  for ((i=1; i<=100; i++)); do
+  count=${2:-100}  # jeśli brak argumentu → domyślnie 100
+  for ((i=1; i<=count; i++)); do
     filename="log$i.txt"
     echo "Nazwa pliku: $filename" > "$filename"
     echo "Utworzony przez: $0" >> "$filename"
