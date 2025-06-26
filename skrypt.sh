be6558d7f3c59bc49022303a6dc67e228a6ab676
michal <dsw59799@student.dsw.edu.pl>
2025-06-26 19:55:39 +0200

    Dodano krótkie flagi oraz instrukcję else, by zapobiec nieprawidłowemu użyciu skryptu

--- skrypt.sh
+++ skrypt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ "$1" == "--date" ]; then
+if [ "$1" == "--date" ] || [ "$1" == "-d" ]; then
   date
 
-elif [ "$1" == "--logs" ]; then
+elif [ "$1" == "--logs" ] || [ "$1" == "-l" ]; then
   count=${2:-100}  # jeśli brak argumentu → domyślnie 100
   for ((i=1; i<=count; i++)); do
     filename="log$i.txt"
@@ -12,9 +12,12 @@ elif [ "$1" == "--logs" ]; then
     echo "Data utworzenia: $(date)" >> "$filename"
   done
 
-elif [ "$1" == "--help" ]; then
+elif [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
   echo "--date         Wyświetla dzisiejszą datę"
   echo "--logs [n]     Tworzy n plików logx.txt (domyślnie 100)"
   echo "--help         Wyświetla listę dostępnych opcji"
+
+else
+  echo "Nieznana opcja. Użyj --help lub -h."
 
 fi
