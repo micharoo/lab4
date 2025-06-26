e3ae68df4f3aab7d0ac5995e9f6e5f408faf138f
michal <dsw59799@student.dsw.edu.pl>
2025-06-26 19:43:19 +0200

    Dodano --logs (domyślnie 100 plików)

--- skrypt.sh
+++ skrypt.sh
@@ -2,4 +2,13 @@
 
 if [ "$1" == "--date" ]; then
   date
+
+elif [ "$1" == "--logs" ]; then
+  for ((i=1; i<=100; i++)); do
+    filename="log$i.txt"
+    echo "Nazwa pliku: $filename" > "$filename"
+    echo "Utworzony przez: $0" >> "$filename"
+    echo "Data utworzenia: $(date)" >> "$filename"
+  done
+
 fi
